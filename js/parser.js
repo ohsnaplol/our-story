f6995790babfd0a6f5e65b45bcc3981ca8daa6ed
Marcello <emyb@bellsouth.net>
2022-01-09 00:24:18 -0500

    create initial story class

--- js/parser.js
+++ js/parser.js
@@ -1,4 +1,5 @@
-const string = `state: left_door.broken,right_door.open
+const string = `
+state: left_door.broken,right_door.open
 interactables:left_door, right_door
 
 // Events syntax👉 on interactable.state action "I'm a great writer, read this!" -> goto room_name or event or interactable.new_state?
@@ -7,39 +8,51 @@ on left_door look:"It is halfway open and smoke is billowing out"
 on left_door.broken open:"It won't budge and its really hot"
 on left_door hit:"It opens up"->left_door.open
 on left_door.open enter:"I walk through with smoke in my face"->goto room_2`
-const COMMAND_DELIMITER = '\n'
-const COMMENT_DELIMITER = '//'
-const KEYWORD_DELIMITER = ':'
 
-const lines = string.split(COMMAND_DELIMITER);
-const cleanLines = lines.filter(line => {
-  if (line == '' || line.startsWith(COMMENT_DELIMITER)) {
-    return false;
+class Story {
+
+  constructor(string) {
+    const COMMAND_DELIMITER = '\n'
+    const COMMENT_DELIMITER = '//'
+    const KEYWORD_DELIMITER = ':'
+
+    this.state = []
+    this.interactables = []
+
+    const lines = string.split(COMMAND_DELIMITER);
+    const cleanLines = lines.filter(line => {
+      if (line == '' || line.startsWith(COMMENT_DELIMITER)) {
+        return false;
+      }
+      return true;
+    })
+    const commands = cleanLines.map(line => {
+      return line.split(KEYWORD_DELIMITER)
+    })
+    commands.forEach((group, index) => {
+      if (group.length != 2) {
+        console.error('Syntax error on line ', group)
+        return;
+      }
+      const keyword = group[0]
+      const action = group[1]
+      if (keyword === 'state') {
+        const states = action.split(',')
+        this.states.push(trimmedStates = states.map(state => state.trim()))
+      }
+      if (keyword === 'interactables') {
+        const interactables = action.split(',')
+        this.interactables.push(interactables.map(interactable => interactable.trim()))
+      }
+      if (keyword.startsWith('on')) {
+        const [on, event, verb] = keyword.split(' ')
+        console.log('event\n', event, verb, '\n', action)
+      }
+    });
+
   }
-  return true;
-})
-const commands = cleanLines.map(line => {
-  return line.split(KEYWORD_DELIMITER)
-})
-commands.forEach((group, index) => {
-  if (group.length != 2) {
-    console.error('Syntax error on line ', group)
-    return;
+
+  loadStates() {
+
   }
-  const keyword = group[0]
-  const action = group[1]
-  if (keyword === 'state') {
-    const states = action.split(',')
-    const trimmedStates = states.map(state => state.trim())
-    console.log('states\n',trimmedStates)
-  }
-  if (keyword === 'interactables') {
-    const interactables = action.split(',')
-    const trimmedInteractables = interactables.map(interactable => interactable.trim())
-    console.log('interactables\n', trimmedInteractables)
-  }
-  if (keyword.startsWith('on')) {
-    const [on, event, verb] = keyword.split(' ')
-    console.log('event\n', event, verb, '\n', action)
-  }
-});
+}
