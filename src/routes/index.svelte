744395ff04fc70776ebc804187d960a9deee50fe
Marcello <emyb@bellsouth.net>
2022-04-22 23:07:37 -0400

    working first option

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,29 +1,48 @@
 <script>
 	import linesStore from '../store';
-  import { onDestroy } from 'svelte'
-  import mystory from '$lib/story.yaml'
+  import { onDestroy } from 'svelte';
+  // @ts-ignore
+  import story from '$lib/story.yaml'
 
 	let lines = [{}];
-	let formValue = '';
 	const unsubscribe = linesStore.subscribe((value) => (lines = value));
 
-  function onChoice(e) {
-    console.log(e.target.value)
-    const text = `You chose ${e.target.value}`
-		linesStore.update((lines) => [...lines, { text, options: { story: [] } }]);
+  eventManager('room_enter')
+
+  function onChoice(e, object) {
+    eventManager(object, e.target.value)
   }
 
-	onDestroy(unsubscribe)
+  function eventManager(event, action) {
+    console.log(event, action)
+    if (!action) {
+      const text = story.events[event]
+      const stringArray = text.split(' ');
+      let options = {}
+      for (let index = 0; index < stringArray.length; index++) {
+        const word = stringArray[index];
+        if (story.events[word]) {
+          options[word] = Object.keys(story.events[word])
+        }
+      }
+      linesStore.update((lines) => [...lines, { text, options }]);
+      return
+    }
+    const text = story.events[event][action];
+    linesStore.update((lines) => [...lines, { text, options: {} }]);
+  }
+
+	onDestroy(unsubscribe);
 </script>
 
 <div id="container">
 	<main id="hud">
 		<ol>
-			{#each lines as line}
+			{#each lines as line, index}
 				<li>
 					{#each line.text.split(' ') as word}
 						{#if line.options[word]}
-							<select on:change={e => onChoice(e)}>
+							<select on:change={e => onChoice(e, word)} disabled={index !== lines.length - 1}>
 								<option>{word}</option>
 								{#each line.options[word] as choice}
 									<option>{choice}</option>
--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,5 @@
 import { writable } from 'svelte/store';
 
-const linesStore = writable([{
-  text: "here is my story",
-  options: {
-    story: ['keyboard', 'mouse']
-  }
-}])
+const linesStore = writable([])
 
 export default linesStore;
